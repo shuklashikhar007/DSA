349c08a8fb54d0d0dc8e17bcbd7b36f0404de455
shuklashikhar007 <shikharjee2023@gmail.com>
2025-07-03 01:10:26 +0530

    BinarySearch

---
+++ BinarySearch/BookAllocationProblem.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the size of the array : " << endl;
+    cin >> n;
+    vector<int>A(n);
+    
+
+    return 0;
+}
