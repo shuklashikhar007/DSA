76b64cb545ce62e88dc077c75ede91aa81b4b3bc
shuklashikhar007 <shikharjee2023@gmail.com>
2025-07-12 17:31:12 +0530

    Some more algo added

--- 2Darray/BasicTheory.cpp
+++ 2Darray/BasicTheory.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 //Not done by now to be done later.
 // Function to print 2D array
-void printArray(int arr[][4], int rows) {
+void printArray(int arr[][4], int rows) { // it's necessary to specify the column size but it's not mandatory to specify the row size
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < 4; j++)
             cout << arr[i][j] << " ";
@@ -26,10 +26,15 @@ int main() {
     printArray(arr, rows);
 
     // Sort each row
-    for (int i = 0; i < rows; i++)
-        sort(arr[i], arr[i] + cols);
+   // for (int i = 0; i < rows; i++)
+     //   sort(arr[i], arr[i] + cols);
+    // if i want to sort the columns as well
 
-    cout << "\nMatrix after sorting each row:\n";
+    for(int i=0;i<cols;i++){
+        sort(arr[i],arr[i] + rows);
+    }
+
+    cout << "\nMatrix after sorting each cols:\n";
     printArray(arr, rows);
 
     return 0;
