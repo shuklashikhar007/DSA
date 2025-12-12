13f15df9c7e46fcd6070b7872767b8a2d4bd8d48
shuklashikhar007 <shikharjee2023@gmail.com>
2025-12-12 10:43:20 +0530

    Wildcard matching

--- DP/StringDP/Wildcardmatch.cpp
+++ DP/StringDP/Wildcardmatch.cpp
@@ -1,6 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool isMatch(string s, string p) {
+    int n = s.size(), m = p.size();
+    vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+    dp[0][0] = true;
+
+    for (int j = 1; j <= m; j++)
+        if (p[j - 1] == '*') dp[0][j] = dp[0][j - 1];
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            if (p[j - 1] == '*')
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+
+            else if (p[j - 1] == '?' || p[j - 1] == s[i - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+
+            else dp[i][j] = false;
+        }
+    }
+    return dp[n][m];
+}
+
 int main(){
-    // AAJ hi karna hai with clarity
+    // AAJ hi karna hai acche se
+    string a,b;
+    cin >> a >> b;
+    int ans = isMatch(a,b);
+    cout << ans << endl;
     return 0;
 }
