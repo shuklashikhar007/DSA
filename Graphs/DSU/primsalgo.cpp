898d51caba0366228fe72f44832724dfb2646c82
shuklashikhar007 <shikharjee2023@gmail.com>
2025-12-25 16:42:11 +0530

    Graph theory end problems begin

---
+++ Graphs/DSU/primsalgo.cpp
@@ -0,0 +1,3 @@
+// Algo to find the wt sum of the minimum spanning tree of a graph
+// MST is nothing but a tree in which all the nodes of the original graph are connected under the condition that
+// all the edges connecting these nodes have their sum of weights a minimum 
