1b8a35eec5603299b177665d12642fb7cdae147c
shuklashikhar007 <shikharjee2023@gmail.com>
2026-03-23 19:05:24 +0530

    Dp ka naya question

--- stackandqueue/monotonicstackproblems/areaoflargestrectangleofallones.cpp
+++ stackandqueue/monotonicstackproblems/areaoflargestrectangleofallones.cpp
@@ -4,33 +4,23 @@ class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         if (matrix.empty()) return 0;
-
         int n = matrix.size(), m = matrix[0].size();
         vector<int> heights(m, 0);
         int maxArea = 0;
-
         for (int i = 0; i < n; i++) {
-
-            // Step 1: Build histogram
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == '1') heights[j] += 1;
                 else heights[j] = 0;
             }
-
-            // Step 2: Apply NSL + NSR histogram logic
             maxArea = max(maxArea, largestRectangleAreaNS(heights));
         }
-
         return maxArea;
     }
-
 private:
     int largestRectangleAreaNS(vector<int>& heights) {
         int n = heights.size();
         vector<int> NSL(n), NSR(n);
         stack<int> st;
-
-        // NSL
         for (int i = 0; i < n; i++) {
             while (!st.empty() && heights[st.top()] >= heights[i]) {
                 st.pop();
@@ -38,9 +28,7 @@ private:
             NSL[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
-        // clear stack
         while (!st.empty()) st.pop();
-        // NSR
         for (int i = n - 1; i >= 0; i--) {
             while (!st.empty() && heights[st.top()] >= heights[i]) {
                 st.pop();
@@ -48,7 +36,6 @@ private:
             NSR[i] = st.empty() ? n : st.top();
             st.push(i);
         }
-        // Compute max area
         int maxArea = 0;
         for (int i = 0; i < n; i++) {
             int width = NSR[i] - NSL[i] - 1;
