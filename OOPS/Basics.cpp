b09ae0031f0a80118093c608244cc36e2dafc50a
shuklashikhar007 <shikharjee2023@gmail.com>
2026-06-27 01:49:40 +0530

    Segmenttree

---
+++ OOPS/Basics.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+    Student(){
+        cout << " HELLO !! " << endl;
+    }
+    ~Student(){
+        cout << "BYE !! " << endl;
+    }
+};
+int main(){
+    Student shi;
+    // Student shikhar(20,80,"white");
+    // cout << shikhar.getage() << endl;
+    // Student shukla(21,82,"brown");
+    // cout << shukla.getcolor() << endl;
+    return 0;
+}
---
+++ SEGMENTTREE.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long; 
+class segmenttree{
+    int n;
+    vector<ll>tree;
+    ll merge(ll lft, ll rgt){
+        return lft + rgt;
+    }
+    public:
+    segmenttree(vector<int>&arr){
+        n = arr.size();
+        tree.resize(4*n);
+        build(0,0,n-1,arr);
+    }
+    void build(int node, int st, int end, vector<int>&arr){
+        if(st == end){
+            tree[node] = arr[st]; // leaf node pe agye base case 
+            return;
+        }
+        int mid  = (st + end)/2;
+        build(2*node+1, st, mid, arr);
+        build(2*node+2,mid+1,end,arr);
+        // pehle build karege fir upar ayege -> bottom up approach
+        tree[node] = merge(tree[2*node+1], tree[2*node+2]);
+        // yaha error isliye nahi ayega since ye dono already generate ho chuke honge
+    }
+    void update(int node, int st, int end, int idx, int val){
+        // index idx par value update karke val karni hai in log(n)
+        if(st == end){
+            // pahuch gye ind wali node par
+            tree[node] = val;
+            return;
+        }
+        int mid = (st+end)/2;
+        if(idx <= mid){ // is case mai search in left half
+            update(2*node+1, st, mid,idx,val);
+        }
+        else{
+            // opposite case mai search in right half
+            update(2*node+2, mid + 1, end, idx, val);
+        }
+        
+        // abb jab wapis return karege after all calls;
+        tree[node] = merge(tree[2*node+1], tree[2*node+2]);
+        // values update karte jayege
+    }
+    ll query(int node, int st, int end, int l , int r){
+        // no overlap wala case
+        if(l > end || r < st){
+            return 0;
+        }
+        // pura overlap wala case return node val
+        else if(l >= st && r <= end){
+            return tree[node];
+        }
+        // partial overlap wala case
+        else{
+            int mid = (st + end)/2;
+            ll left = query(2*node+1,st,mid,l,r);
+            ll right = query(2*node+2,mid+1,end,l,r);
+            return merge(left,right); // return sum of both left and right wale case
+        }
+    }
+    void update(int idx, int val){
+        update(0,0,n-1,idx,val);
+    }
+    ll query(int l, int r){
+        return query(0,0,n-1,l,r);
+    }
+};
+int main(){
+    // same code can be used to solve max min in range wali problem as well
+    return 0;
+}
